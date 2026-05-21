9d5a4d8f5dc398beca1c434d568c3dcd3b771daf
sxexesx <sxexesx@gmail.com>
2026-05-21 19:04:23 +0300

    add examples

--- _examples/ex1/main.go
+++ _examples/ex1/main.go
@@ -1,19 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
+
+	"golang.org/x/tools/go/analysis/passes/defers"
 )
 
-func main() {
-	for i := 0; i < 6; i++ {
-		go networkRequest(i)
-	}
+type Data struct {
+	threadID int
+	data     int
 }
 
-func networkRequest(threadID int) {
-	timeout := rand.Intn(20)
-	fmt.Println("[", threadID, "]", " Waiting for ", timeout, " seconds.")
+// var bufferPool = sync.Pool{
+// 	New: func() any {
+// 		return 10
+// 	},
+// }
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	data := make(chan Data)
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(10)*time.Second)
+	defer cancel()
+
+	for i := 0; i < 3; i++ {
+		go networkRequest(&wg, i, data)
+	}
+
+	go func() {
+		wg.Wait()
+		close(data)
+	}()
+
+	for v := range data {
+		fmt.Println("[", v.threadID, "] ", "Finish waiting for ", v.data, " seconds. ", " Buffer pool value: ", bufferPool.Get().(int))
+	}
+
+}
+
+func networkRequest(wg *sync.WaitGroup, threadID int, data chan Data) {
+	defer wg.Done()
+
+	// a := bufferPool.Get().(int)
+	timeout := rand.Intn(30)
+
+	// fmt.Println("[", threadID, "] ", "Executing sync.Pool value: ", a, " Random timout: ", timeout)
+
+	// if timeout > a {
+	// 	timeout = a
+	// } else {
+	// 	fmt.Println("[", threadID, "] ", "Putting value into sync.Pool ", timeout)
+	// 	bufferPool.Put(timeout)
+	// }
+
+	fmt.Println("[", threadID, "] ", "Waiting for ", timeout, " seconds.")
 	time.Sleep(time.Duration(timeout) * time.Second)
+
+	data <- Data{
+		threadID: threadID,
+		data:     timeout,
+	}
 }
---
+++ _examples/ex2/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+func main() {
+
+}
+
+type KVAgent struct {
+	client Client
+}
+
+type Client interface {
+	Get(ctx context.Context, key string) (result string, err error)
+}
+
+type ResultWithError struct {
+	result string
+	err    error
+}
+
+func (k *KVAgent) GetHedged(
+	ctx context.Context,
+	key string,
+	hedgeDelay time.Duration,
+	maxDelay time.Duration,
+) (result string, err error) {
+
+	ctx, cancel := context.WithTimeout(ctx, maxDelay)
+	defer cancel()
+
+	// master + 2 replicas
+	resultCh := make(chan ResultWithError, 3)
+
+	// отправляем запрос на мастер
+	go func() {
+		res, err := k.client.Get(ctx, key)
+		resultCh <- ResultWithError{
+			result: res,
+			err:    err,
+		}
+	}()
+
+	select {
+	case v := <-resultCh:
+		return v.result, v.err
+	case <-time.After(hedgeDelay):
+	}
+
+	for i := 0; i < 2; i++ {
+		go func() {
+			res, err := k.client.Get(ctx, key)
+			resultCh <- ResultWithError{result: res, err: err}
+		}()
+	}
+
+	select {
+	case v := <-resultCh:
+		return v.result, v.err
+	case <-ctx.Done():
+	}
+	return "", errors.New("timeout")
+}
